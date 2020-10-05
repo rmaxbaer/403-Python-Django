762b5097c078858f4f1ce30d3fbf7758a4de7de4
Max Baer <rmaxbaer@gmail.com>
2020-10-05 12:38:03 -0600

    appointment data collection from user

--- OOP_Assignment/oop_program.py
+++ OOP_Assignment/oop_program.py
@@ -1,4 +1,8 @@
+# import our classes 
 from oop_class_structure import Customer, Pet, Appointment
+from datetime import datetime
+
+# 
 first_name = input("Enter first name: ")
 last_name = input("Enter last name: ")
 address1 = input("Enter address line 1: ")
@@ -12,4 +16,17 @@ pet_name = input("Enter your pet's name: ")
 breed = input("Enter your pet's breed: ")
 age = int(input("Enter your pet's age: "))
 pet1 = Pet(pet_name, breed, age, customer1)
+
+begin_date =  datetime.strptime(input("Enter Start date in the format m/d/y: "), "%m/%d/%Y")
+end_date = datetime.strptime(input("Enter End date in the format m/d/y: "), "%m/%d/%Y")
+day_rate = float(input("Enter the day rate: "))
+
+pet1.appointment.set_appointment(begin_date, end_date, day_rate)  
+
+print(customer1.return_bill())
+
+payment = float(input("Enter the payment amount: "))
+
+customer1.make_payment(payment)
+
 print(customer1.return_bill())

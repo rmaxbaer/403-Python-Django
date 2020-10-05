d4b67ab90554354ecfdd4864be56bd672ca0f41c
Max Baer <rmaxbaer@gmail.com>
2020-10-05 11:15:20 -0600

    Added Pet and Appointment classes

--- OOP_Assignment/oopgroupassignment.py
+++ OOP_Assignment/oopgroupassignment.py
@@ -1,20 +1,39 @@
 class Customer:
     company_name = "Critter Watch"
     def __init__(self, first_name, last_name, address1, address2, city, state, zip):
-        self.cust_id = ""
-        self.first_name = ""
-        self.last_name = ""
-        self.address1 = ""
-        self.address2 = ""
-        self.city = ""
-        self.state = ""
-        self.zip =""
+        self.cust_id = self.gen_id(first_name, last_name, address1)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address1 = address1
+        self.address2 = address2
+        self.city = city
+        self.state = state
+        self.zip = zip
         self.balance = 0.0
         self.cust_pet = None
-    def gen_id(self, first_name, last_name, address1) :
-        pass
-    def return_bill (self) :
-        pass
-    def make_payment(self, pmt) :
+    def gen_id(self, first_name, last_name, address1):
+        return ""
+    def return_bill (self):
+        return ""
+    def make_payment(self, pmt):
         pass
     
+class Pet:
+    def __init__(self, name, breed, age, owner):
+        self.pet_name = name
+        self.breed = breed
+        self.age = age
+        self.appointment = None
+
+class Appointment:
+    def __init__(self, owner):
+        self.begin_date = None
+        self.end_date = None
+        self.day_rate = 0.0
+        self.total_days = 0
+        self.total_cost = 0.0
+        self.owner = owner
+    def calc_days(self):
+        pass
+    def set_appointment(self, begin_date, end_date, day_rate):
+        pass
